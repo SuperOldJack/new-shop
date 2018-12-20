23521720a28457c80b036d72683c07a4e04909fb
周杰 <1316567457@qq.com>
2018-12-20 14:58:02 +0800

    补上

---
+++ sns/src/main/resources/static/js/websocket/orderGoodsSocket.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+var ws;
+
+if ("WebSocket" in window)
+{
+	alert("您的浏览器支持 WebSocket!");
+
+	// 打开一个 web socket
+	ws = new WebSocket("ws://localhost:80/orderGoodsSelect");
+
+	ws.onopen = function()
+	{
+		alert("数据发送中...");
+	};
+
+	ws.onmessage = function (evt) 
+	{ 
+		var received_msg = evt.data;
+		alert("数据已接收...");
+		document.getElementById('message').innerHTML += received_msg + '<br/>';
+	};
+
+	ws.onclose = function()
+	{ 
+		// 关闭 websocket
+		alert("连接已关闭..."); 
+	};
+}
+
+else
+{
+	// 浏览器不支持 WebSocket
+	alert("您的浏览器不支持 WebSocket,将无法使用大部分功能,请更换浏览器");
+}
+function sendMessage(){
+
+	ws.send(document.getElementById('text').value);
+}
